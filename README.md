2921f53c7de22964a7de483dd7f252a9a1d992fc
ahavens1995 <87989306+ahavens1995@users.noreply.github.com>
2024-06-21 16:32:18 -0700

    Cleanup and Debugging
    Test light and dark mode. Found a bug where the modes were not continuing between pages. This is fixed now. Added a readme with details on the project

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Havens-Blog-Website
+
+## Table of Contents
+- [Description](#description)
+    - [Built With](#built-with)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [License](#license)
+- [Contact](#contact)
+
+## Description
+This project allows a user to submit a blog post with their username, blog title, and post. When the user submits a post, they will be redirected to the blog page where they can view previous posts stored in local memory. There is also a bulb allowing users to toggle the theme from dark to light.
+
+https://ahavens1995.github.io/Havens-Blog-Website/
+
+![image](./assets/images/splashPage.PNG)
+
+### Built with
+Project built with:
+* JavaScript
+* HTML
+* CSS
+
+## Getting Started 
+To use the payroll tracker, perform the following:
+1. Open the github page: https://ahavens1995.github.io/Havens-Blog-Website/
+    ![image](./assets/images/splashPage.PNG)
+2. Enter the following:
+    * Name
+    * Blog Title
+    * Blog Post \
+        ![image](./assets/images/filledOutHome.PNG)
+3. After you add all the information, select "Send Message"
+4. You will be redirected to the blog page where you can view all posts stored in local memory
+    ![image](./assets/images/blogPage.PNG)
+5. To change the theme on either page, select the light bulb button in the upper right corner \
+    ![image](./assets/images/lightbulbCorner.PNG)
+6. Here is light mode:
+    ![image](./assets/images/lightModeOn.PNG)
+    ![image](./assets/images/lightmodeOnHome.PNG)
+
+## Usage
+Please use this as an initial look into how Javascript functions and methods interact with HTML and CSS.
+
+## License
+No license has been used
+
+## Contact
+Aurora Havens
+https://ahavens1995.github.io/Havens-Blog-Website/
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,10 +7,16 @@ let mode = 'dark';
 
 // Listen for a click event on toggle switch
 themeSwitcher.addEventListener('click', function () {
+
+  const storedMode = localStorage.getItem('mode');
   
+  if(storedMode !== null){
+    mode = storedMode;
+  }
+
   // Grab the image target in order to change the light bulb image
   const element = event.target;
-  const state = element.getAttribute('data-state');
+  //const state = element.getAttribute('data-state');
 
   // If mode is dark, apply light background
   if (mode === 'dark') {
@@ -21,6 +27,7 @@ themeSwitcher.addEventListener('click', function () {
     element.dataset.state='light';
     element.setAttribute('data-state', 'light');
     element.setAttribute('src', element.dataset.light);
+
   }
   // If mode is light, apply dark background
   else {
@@ -32,4 +39,44 @@ themeSwitcher.addEventListener('click', function () {
     element.setAttribute('data-state', 'dark');
     element.setAttribute('src', element.dataset.dark);
   }
+
+  localStorage.setItem('mode', mode);
+
 });
+
+function renderMode(){
+
+  const storedMode = localStorage.getItem('mode');
+
+  //const state = element.getAttribute('data-state');
+
+  // If the stored mode is Dark, apply the dark background
+  if (storedMode === 'dark') {
+
+    container.setAttribute('class', 'dark');
+
+    // Change the light bulb image to Off
+    themeSwitcher.dataset.state='dark';
+    themeSwitcher.setAttribute('data-state', 'dark');
+    themeSwitcher.setAttribute('src', element.dataset.dark);
+
+  }
+  // If stored mode is light, apply light background
+  else {
+
+    container.setAttribute('class', 'light');
+
+    // Change the light bulb image to On
+    themeSwitcher.dataset.state='light';
+    themeSwitcher.setAttribute('data-state', 'light');
+    themeSwitcher.setAttribute('src', themeSwitcher.dataset.light);
+  }
+
+
+}
+
+function init(){
+  renderMode();
+}
+
+init();
